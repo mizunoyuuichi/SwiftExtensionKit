56522611384915692097897dbcc380a2fedb39d2
mizen <yuuch_w@hotmail.com>
2025-04-19 13:08:11 +0900

    update

---
+++ Sources/SwiftExtensionKit/Views/SwiftUI/Color.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  SwiftExtensionKit
+//
+//  Created by Yuuichi Mizuno on 2025/02/21.
+//
+
+import SwiftUI
+
+extension View {
+
+	static var grayScale0 : Color { Color.init(hue: 0, saturation: 0, brightness: 0) }
+	static var grayScale10: Color { Color.init(hue: 0, saturation: 0, brightness: 0.1) }
+	static var grayScale20: Color { Color.init(hue: 0, saturation: 0, brightness: 0.2) }
+	static var grayScale30: Color { Color.init(hue: 0, saturation: 0, brightness: 0.3) }
+	static var grayScale40: Color { Color.init(hue: 0, saturation: 0, brightness: 0.4) }
+	static var grayScale50: Color { Color.init(hue: 0, saturation: 0, brightness: 0.5) }
+	static var grayScale60: Color { Color.init(hue: 0, saturation: 0, brightness: 0.6) }
+	static var grayScale70: Color { Color.init(hue: 0, saturation: 0, brightness: 0.7) }
+	static var grayScale80: Color { Color.init(hue: 0, saturation: 0, brightness: 0.8) }
+	static var grayScale90: Color { Color.init(hue: 0, saturation: 0, brightness: 0.9) }
+	static var grayScale100: Color { Color.init(hue: 0, saturation: 0, brightness: 1.0) }
+
+}
