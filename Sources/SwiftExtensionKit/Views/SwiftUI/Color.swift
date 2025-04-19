946cac8997b6d12a7efb2e834d5b1355fa0b671d
mizen <yuuch_w@hotmail.com>
2025-04-19 13:15:32 +0900

    u

--- Sources/SwiftExtensionKit/Views/SwiftUI/Color.swift
+++ Sources/SwiftExtensionKit/Views/SwiftUI/Color.swift
@@ -1,24 +1,33 @@
 //
-//  File.swift
 //  SwiftExtensionKit
-//
-//  Created by Yuuichi Mizuno on 2025/02/21.
+//  Created by Yuuichi Mizuno
 //
 
 import SwiftUI
 
-extension View {
+extension Color {
 
-	static var grayScale0 : Color { Color.init(hue: 0, saturation: 0, brightness: 0) }
-	static var grayScale10: Color { Color.init(hue: 0, saturation: 0, brightness: 0.1) }
-	static var grayScale20: Color { Color.init(hue: 0, saturation: 0, brightness: 0.2) }
-	static var grayScale30: Color { Color.init(hue: 0, saturation: 0, brightness: 0.3) }
-	static var grayScale40: Color { Color.init(hue: 0, saturation: 0, brightness: 0.4) }
-	static var grayScale50: Color { Color.init(hue: 0, saturation: 0, brightness: 0.5) }
-	static var grayScale60: Color { Color.init(hue: 0, saturation: 0, brightness: 0.6) }
-	static var grayScale70: Color { Color.init(hue: 0, saturation: 0, brightness: 0.7) }
-	static var grayScale80: Color { Color.init(hue: 0, saturation: 0, brightness: 0.8) }
-	static var grayScale90: Color { Color.init(hue: 0, saturation: 0, brightness: 0.9) }
-	static var grayScale100: Color { Color.init(hue: 0, saturation: 0, brightness: 1.0) }
+	static func random() -> Color {
+		return Color(
+			red: .random(in: 0...0.5),
+			green: .random(in: 0...0.5),
+			blue: .random(in: 0...1)
+		).opacity(0.5)
+	}
+}
 
+
+extension Color {
+
+	static var monoTone0  : Color { Color.init(hue: 0, saturation: 0, brightness: 0) }
+	static var monoTone10 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.1) }
+	static var monoTone20 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.2) }
+	static var monoTone30 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.3) }
+	static var monoTone40 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.4) }
+	static var monoTone50 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.5) }
+	static var monoTone60 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.6) }
+	static var monoTone70 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.7) }
+	static var monoTone80 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.8) }
+	static var monoTone90 : Color { Color.init(hue: 0, saturation: 0, brightness: 0.9) }
+	static var monoTone100: Color { Color.init(hue: 0, saturation: 0, brightness: 1.0) }
 }
