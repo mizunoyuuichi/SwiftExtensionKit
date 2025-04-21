acf4cb67e496fb2db77d877f76a3e3d26fe4302c
mizen <yuuch_w@hotmail.com>
2025-04-21 14:46:34 +0900

    u

--- Sources/SwiftExtensionKit/Extensions/Foundation/Range.swift
+++ Sources/SwiftExtensionKit/Extensions/Foundation/Range.swift
@@ -4,7 +4,7 @@ import Foundation
 import CoreFoundation
 import CoreGraphics
 
-public extension Range where Element: Equatable , Comparable {
+public extension Range where Element: Equatable & Comparable {
 
 	// MARK: - cast
 	var array: [Bound] {
