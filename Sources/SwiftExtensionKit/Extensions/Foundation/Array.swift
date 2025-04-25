5ffa3e17e00fc282a72579cd1bc5e39af3337b25
mizen <yuuch_w@hotmail.com>
2025-04-25 19:24:57 +0900

    u

--- Sources/SwiftExtensionKit/Extensions/Foundation/Array.swift
+++ Sources/SwiftExtensionKit/Extensions/Foundation/Array.swift
@@ -4,6 +4,22 @@ import Foundation
 
 public extension Array {
 
+	var anyObject: AnyObject {
+		self as AnyObject
+	}
+
+	var isPresent: Bool {
+		0 < self.count
+	}
+
+	var isSingle: Bool {
+		self.count == 1
+	}
+
+	var isEmpty: Bool {
+		self.count <= 0
+	}
+
 	func isInRange(_ index: Int) -> Bool {
 		index < self.count
 	}
@@ -11,5 +27,13 @@ public extension Array {
 	func withinRange(_ row: Int) -> Element? {
 		guard 0 <= row else { return nil }
 		return isInRange(row) ? self[row] : nil
+	}
+
+	func head(_ row: Int) -> Array {
+		Array(prefix(row))
+	}
+
+	func tail(_ row: Int) -> Array {
+		suffix(row)
 	}
 }
