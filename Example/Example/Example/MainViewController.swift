ff4485feeb19e24fc26d8bffbba11d93cbfe4c13
mizen <yuuch_w@hotmail.com>
2026-05-22 14:07:46 +0900

    add - sample project

---
+++ Example/Example/Example/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Example
+//
+//  Created by Yuuichi Mizuno on 2026/05/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
