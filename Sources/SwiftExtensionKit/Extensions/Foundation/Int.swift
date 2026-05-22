80662142482059fd970d0e3749d2c7edd8cddd30
mizen <yuuch_w@hotmail.com>
2026-05-22 14:27:31 +0900

    update

--- Sources/SwiftExtensionKit/Extensions/Foundation/Int.swift
+++ Sources/SwiftExtensionKit/Extensions/Foundation/Int.swift
@@ -4,61 +4,78 @@ import Foundation
 import CoreFoundation
 import CoreGraphics
 
+
+
+// MARK: - cast
 public extension Int {
 
-	// MARK: - debug
-	var random: Int {
-		Int.random(in: 0...self)
-	}
+    var cgfloat: CGFloat { CGFloat(self) }
 
-	// MARK: - cast
-	var cgfloat: CGFloat {
-		CGFloat(self)
-	}
+    var string : String  { String(self) }
 
-	var string: String {
-		String(self)
-	}
+    var range  : Range<Int> { 0..<self }
 
-	var range: Range<Int> {
-		0..<self
-	}
 
-	// MARK: - compar
-	var isPositive: Bool {
-		0 < self
-	}
+    var second : TimeInterval { TimeInterval(self) }
 
-	var isNegativeOrNeutral: Bool {
-		self <= 0
-	}
+    var minutes: TimeInterval { TimeInterval(self * 60) }
 
-	var isNeutral: Bool {
-		self == 0
-	}
+    var hours  : TimeInterval { TimeInterval(self) * 60.minutes }
 
-	var isNegative: Bool {
-		self < 0
-	}
+    var days   : TimeInterval { TimeInterval(self) * 24.hours }
 
-	var plus1: Int {
-		self + 1
-	}
+    var month  : TimeInterval { TimeInterval(self) * 30.days }
+}
 
-	// MARK: - format
-	var separated: String {
-		get {
-			let isMinus = self < 0
-			let formatter = NumberFormatter()
-				formatter.numberStyle = .decimal
-				formatter.groupingSeparator = ","
-				formatter.groupingSize = 3
-			let formated = formatter.string(from: NSNumber(value: abs(self))) ?? "0"
-			return isMinus ? "-\(formated)" : "\(formated)"
-		}
-	}
 
-	var jpYen: String {
-		"¥\(separated)"
-	}
+
+// MARK: - debug
+public extension Int {
+
+    var random : Int { Int.random(in: 0...self) }
+}
+
+
+
+
+// MARK: - format
+public extension Int {
+
+    var separated: String {
+        get {
+            let isMinus = self < 0
+            let formatter = NumberFormatter()
+                formatter.numberStyle = .decimal
+                formatter.groupingSeparator = ","
+                formatter.groupingSize = 3
+            let formated = formatter.string(from: NSNumber(value: abs(self))) ?? "0"
+            return isMinus ? "-\(formated)" : "\(formated)"
+        }
+    }
+
+    var jpYen: String { "¥\(separated)" }
+}
+
+
+
+// MARK: - compar
+public extension Int {
+
+    var isPositive: Bool { 0 < self }
+
+    var isNegativeOrNeutral: Bool { self <= 0 }
+
+    var isNeutral: Bool { self == 0 }
+
+    var isNegative: Bool { self < 0 }
+
+    var plus1: Int { self + 1 }
+
+    func equalOrMoreThan(_ target: Int) -> Bool {
+        return (target <= self)
+    }
+
+    func lessThan(_ target: Int) -> Bool {
+        return self < target
+    }
 }
