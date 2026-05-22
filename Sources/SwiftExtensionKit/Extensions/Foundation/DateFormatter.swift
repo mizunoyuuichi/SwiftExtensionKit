508f32b0fa95392aee85a2bb6162c864482ce7fc
mizen <yuuch_w@hotmail.com>
2026-05-22 14:34:50 +0900

    add

---
+++ Sources/SwiftExtensionKit/Extensions/Foundation/DateFormatter.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let kServerDateTimeFormat = String("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ")
+let kDateTimeOmitSecondsFormat = String("yyyy/MM/dd HH:mm")
+let kDateFormat = String("yyyy-MM-dd")
+
+extension DateFormatter {
+
+    func japaneaseDefaultTime() -> DateFormatter {
+        timeZone = TimeZone(identifier: "Asia/Tokyo")
+        locale = Locale(identifier: "ja_JP")
+        calendar = Calendar(identifier: .gregorian)
+        return self
+    }
+
+    func set(dateFormat: String) -> DateFormatter {
+        self.dateFormat = dateFormat
+        return self
+    }
+
+    func serverDateTimeFormat() -> DateFormatter {
+        return set(dateFormat: kServerDateTimeFormat)
+    }
+
+    func dateTimeFormatOmitSeconds() -> DateFormatter {
+        return set(dateFormat: kDateTimeOmitSecondsFormat)
+    }
+
+    func dateFormat() -> DateFormatter {
+        return set(dateFormat: kDateFormat)
+    }
+
+}
