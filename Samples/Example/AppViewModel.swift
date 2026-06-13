8058cbe55b4a5ed18aeb5c02a561b211c0708c6e
mizen <yuuch_w@hotmail.com>
2026-06-13 15:45:59 +0900

    add

---
+++ Samples/Example/AppViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AppViewModel.swift
+//  Example
+//
+//  Created by Yuuichi Mizuno on 2026/06/13.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+@Observable
+final class AppViewModel {
+
+    static let shared = AppViewModel()
+
+    private init () {}
+}
