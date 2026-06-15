c5f5e086775b4426ca1d8fdaee5e9c0e891e62a6
mizen <yuuch_w@hotmail.com>
2026-06-15 11:34:06 +0900

    update

--- Sources/SwiftExtensionKit/Extensions/SwiftUI/Text.swift
+++ Sources/SwiftExtensionKit/Extensions/SwiftUI/Text.swift
@@ -1,44 +1,99 @@
 import SwiftUI
 
-// MARK: - style
+// MARK: - level
 public extension Text {
     /// 同じようなテキストの規格化 (純正のものは使いにくそうなので独自に用意)
     /// 基本5つ : { head, body, label, caption, footnote }
-    ///            見出し, 本文, UI部品, 補足, 最小の注釈
 
-    // MARK: 基本形
-    func asHead(isActive: Bool = true, isSelected: Bool = false) -> some View {
+    /// 見出し
+    func asHead(isActive: Bool = true, isSelected: Bool = false) -> Text {
         self
             .kerning(-0.2)
-            .font(.system(size: 16))  // ここも上書きできる値参照させるか、、
+            .font(.system(size: 18))  // ここも上書きできる値参照させるか、、
             .fontWeight(isSelected ? .medium : .regular)
-            .minimumScaleFactor(0.5)
-            .lineLimit(2)
-            .foregroundColor(isActive ? .k20 : .k50)
-            .frame(maxWidth: .infinity, alignment: .leading)
+            //.lineLimit(2)
+            //.minimumScaleFactor(0.5)
+            .foregroundStyle(isActive ? Color.k20 : Color.k50)
     }
-
-    func asCaption(isStraightLine: Bool = false) -> some View {
+    /// 本文
+    func asBody(isStraightLine: Bool = false, alignment: Alignment = .leading) -> Text {
         self
-            .font(.system(size: 11, weight: .medium))
+            .font(.system(size: 16, weight: .medium))
+            .foregroundStyle(Color.k40)
+            //.minimumScaleFactor(0.5)
+            //.lineLimit(isStraightLine ? 1 : nil)
+            //.lineSpacing(2) as! Text
+
+            //.frame(maxWidth: .infinity, alignment: alignment)
+    }
+    /// UI部品
+    func asLabel() -> Text {
+        self
+    }
+    /// 補足
+    func asCaption(isStraightLine: Bool = false, alignment: Alignment = .leading) -> some View {
+        self
+            .font(.system(size: 14, weight: .medium))
             .minimumScaleFactor(0.5)
             .lineLimit(isStraightLine ? 1 : nil)
             .lineSpacing(2)
             .foregroundColor(.k40)
-            .frame(maxWidth: .infinity, alignment: .leading)
+            .frame(maxWidth: .infinity, alignment: alignment)
+    }
+    /// 最小の注釈
+    func asFootnote(isStraightLine: Bool = false, alignment: Alignment = .leading) -> some View {
+        self
+            .font(.system(size: 12, weight: .medium))
+            .minimumScaleFactor(0.5)
+            .lineLimit(isStraightLine ? 1 : nil)
+            .lineSpacing(2)
+            .foregroundColor(.k40)
+            .frame(maxWidth: .infinity, alignment: alignment)
     }
 
     // MARK: 特殊系
-    func asDetailLink(isActive: Bool = true) -> some View {
+    func asDetailLink(isActive: Bool = true, alignment: Alignment = .leading) -> some View {
         self
             .font(.system(size: 10, weight: .medium))
             .minimumScaleFactor(0.5)
             .lineLimit(1)
             .foregroundColor(isActive ? .blue : .blue.opacity(0.6))
-            .frame(maxWidth: .infinity, alignment: .leading)
+            .frame(maxWidth: .infinity, alignment: alignment)
             .padding(.top, 4)
     }
 }
+
+
+// MARK: - style
+public extension Text {
+
+    /// 基本4つ { airily, relaxedly, formally, tightly }
+
+    // 軽やかに
+    func airily() -> some View {
+        self
+    }
+
+    // ゆったりと
+    func relaxedly() -> some View {
+        self
+    }
+
+    // きちんと
+    func formally() -> some View {
+        self
+    }
+
+    // しっかりだけど詰め気味
+    func tightly() -> some View {
+        self
+        .kerning(-0.2)
+        //.lineLimit(2)
+        //.lineSpacing(2)
+        //.minimumScaleFactor(0.5)
+    }
+}
+
 
 
 
