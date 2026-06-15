097656296d7409d456a6f1a444727bd2289c8fb7
mizen <yuuch_w@hotmail.com>
2026-06-15 11:33:49 +0900

    update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftExtensionKit",
 	platforms: [
-		.iOS(.v16), .macOS(.v14), .tvOS(.v13), .watchOS(.v6)
+		.iOS(.v17), .macOS(.v14), .tvOS(.v13), .watchOS(.v6)
 	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
