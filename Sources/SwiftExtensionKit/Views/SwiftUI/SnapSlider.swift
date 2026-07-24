0945bb1a4a4704d37c42a4021d937390a7a056ac
mizen <45590393+mizunoyuuichi@users.noreply.github.com>
2026-07-24 19:24:44 +0900

    Update SnapSlider.swift

--- Sources/SwiftExtensionKit/Views/SwiftUI/SnapSlider.swift
+++ Sources/SwiftExtensionKit/Views/SwiftUI/SnapSlider.swift
@@ -1,27 +1,27 @@
 import SwiftUI
 
 public struct SnapSlider: View {
-    @State private var value : Double
-    @State private var max   : Double
-    @State private var step  : Double
+    @State private var current : Double
+    @State private var max     : Double
+    @State private var step    : Double
 
     public var onChanged: ((Double) -> Void)?
 
-    public init(value: Double = 5,
-         max  : Double = 10,
-         step : Double = 1) {
-        self.value = value
-        self.max   = max
-        self.step  = step
+    public init(current: Double = 5,
+                max  : Double = 10,
+                step : Double = 1) {
+        self.current = current
+        self.max     = max
+        self.step    = step
     }
 
     public var body: some View {
         Slider(
-            value: $value,
+            value: $current,
             in: 0...max,
             step: step
         )
-        .onChange(of: value) { old, new in
+        .onChange(of: current) { old, new in
             onChanged?(new)
         }
     }
