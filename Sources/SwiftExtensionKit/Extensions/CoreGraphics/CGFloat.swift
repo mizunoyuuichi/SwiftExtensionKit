2f10cf9b5e1a5824a1de50b71887412bbbd6e74a
mizen <45590393+mizunoyuuichi@users.noreply.github.com>
2026-08-25 11:03:31 +0900

    Update CGFloat.swift

--- Sources/SwiftExtensionKit/Extensions/CoreGraphics/CGFloat.swift
+++ Sources/SwiftExtensionKit/Extensions/CoreGraphics/CGFloat.swift
@@ -1,12 +1,34 @@
+//  Created by Mizuno Yuichi
+
 import Foundation
 
 extension CGFloat {
 
     static var one : CGFloat { CGFloat(1.0) }
 
-    static var zero: CGFloat { CGFloat(0) }
+    static var zero: CGFloat { CGFloat(0.0) }
+
+
+    var double: CGFloat { self * 2 }
 
     var roundedDown: CGFloat { CGFloat(floor(self)) }
 
     var roundedUp  : CGFloat { CGFloat(ceil(self)) }
+
+
+    func minus(_ subject: CGFloat) -> CGFloat {
+        self - subject
+    }
+
+    func limit(min subject: CGFloat) -> CGFloat {
+        (self >= subject)
+            ? subject
+            : self
+    }
+
+    func limit(max subject: CGFloat) -> CGFloat {
+        (self <= subject)
+            ? self
+            : subject
+    }
 }
